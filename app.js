48ec6b91a3332f435c7f1484dee117778de50771
Rufus-Sam <sam2001rufus@gmail.com>
2022-09-05 10:38:48 +0530

    Mongodb cloud connected

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI).then(()=> console.log("Mongodb connected"))
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT;
+app.use(express.json())
 
 app.listen(PORT,()=>{
     console.log("Server Started");
