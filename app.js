2463bd22331d6cdad4a9648a67c94a8f0e597bcd
Rufus-Sam <sam2001rufus@gmail.com>
2022-09-05 10:54:01 +0530

    created blog schema

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const dotenv = require('dotenv');
+dotenv.config()
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-dotenv.config()
 mongoose.connect(process.env.MONGO_URI).then(()=> console.log("Mongodb connected"))
+const blogs = require('./routes/blogs')
+
 
 const PORT = process.env.PORT;
 app.use(express.json())
+app.use('/blogs',blogs);
+
+app.get('/',(req,res) =>{
+    res.redirect('/blogs');
+})
+
 
 app.listen(PORT,()=>{
     console.log("Server Started");
