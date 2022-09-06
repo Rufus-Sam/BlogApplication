14cd3a937549785b6b8693234009fe0b063479e0
Rufus-Sam <sam2001rufus@gmail.com>
2022-09-06 21:19:57 +0530

    Trying join two servers in express

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
+//to store secret keys
 const dotenv = require('dotenv');
 dotenv.config()
+
+//start express 
+// backend , frontend server using cors
 const express = require('express');
-const app = express();
+const backend = express();
+const frontend = express();
+const cors = require('cors')
+backend.use(cors());
+frontend.use(cors());
+// database connect
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI).then(()=> console.log("Mongodb connected"))
-const blogs = require('./routes/blogs')
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Mongodb connected"))
+//import needed files
+const blogs = require('./backendroutes/blogs')
 
-
-const PORT = process.env.PORT;
-app.use(express.json())
-app.use('/blogs',blogs);
-
-app.get('/',(req,res) =>{
+backend.use(express.json());
+backend.use('/blogs', blogs);
+backend.get('/', (req, res) => {
     res.redirect('/blogs');
+})
+frontend.use(express.static('public'))
+frontend.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 
-app.listen(PORT,()=>{
-    console.log("Server Started");
+frontend.listen(process.env.FRONT_PORT, () => {
+    console.log("Frontend started");
+})
+
+backend.listen(process.env.BACKEND_PORT, () => {
+    console.log("Backend started");
 })
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HomePage from './screens/HomePage'
+
+
+ReactDOM.render(<HomePage />, document.getElementById('root'))
